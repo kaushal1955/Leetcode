e6441cf32ee3b4d203650021c6cce4f2e8caf03a
Kaushal <kaushalkumar01956@gmail.com>
2026-07-07 11:57:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (10.85%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555);
+        if (n <= 0)
+            return false;
+
+        while (n % 4 == 0)
+            n /= 4;
+
+        return n == 1;
     }
 };
