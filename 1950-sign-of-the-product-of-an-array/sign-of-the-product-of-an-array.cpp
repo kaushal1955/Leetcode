ceb7ebf3a1c944b34d8f27d717f8c611d7e85d4e
Kaushal <kaushalkumar01956@gmail.com>
2026-07-10 00:09:23 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (51.51%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        int countNegativeNumbers = 0;
-        for (int num : nums) {
-            if (num == 0) {
+        int sign =1;
+        for(int num:nums){
+            if(num==0){
                 return 0;
             }
-            if (num < 0) {
-                countNegativeNumbers++;
+            if(num<0){
+                sign= -1*sign;
             }
         }
-
-        return countNegativeNumbers % 2 == 0 ? 1 : -1;
+        return sign;
     }
 };
