0df11e692d3fa37d05434bef6b1ebf79d900957a
Kaushal <kaushalkumar01956@gmail.com>
2026-08-04 01:27:22 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (81.93%) - LeetSync

---
+++ 2650-split-with-minimum-sum/split-with-minimum-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int splitNum(int num) {
+        string s= to_string(num);
+        sort(s.begin(),s.end());
+        int a=0,b=0;
+        for(int i=0;i<s.size();i++){
+            if(i%2==0){
+                a= a*10+(s[i]-'0');
+            }else{
+                b=b*10+(s[i]-'0');
+            }
+        }
+        return a+b;
+    }
+};
+
