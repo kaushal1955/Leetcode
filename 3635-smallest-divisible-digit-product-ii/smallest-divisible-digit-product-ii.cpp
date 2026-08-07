6e2afe4f1bf7c13113ae52905fdf05c023a22aaf
Kaushal <kaushalkumar01956@gmail.com>
2026-08-07 17:58:25 +0530

    Time: 47 ms (65.52%) | Memory: 52.4 MB (48.28%) - LeetSync

--- 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
+++ 3635-smallest-divisible-digit-product-ii/smallest-divisible-digit-product-ii.cpp
@@ -1,72 +1,84 @@
-#include <string>
-#include <vector>
-#include <numeric>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
-    std::string smallestNumber(std::string num, long long t) {
+    string smallestNumber(string num, long long t) {
         long long temp = t;
-        for (int i = 2; i <= 9; ++i) {
-            while (temp % i == 0) temp /= i;
+        for (int i = 2; i <= 9; i++) {
+            while (temp % i == 0)
+                temp /= i;
         }
-        if (temp > 1) return "-1";
+
+        if (temp > 1)
+            return "-1";
 
         int n = num.length();
-        std::vector<long long> rem(n + 1, 0);
-        rem[0] = t;
-        int pos = n - 1;
+        vector<long long> rem(n + 1, t);
 
-        for (int i = 0; i < n; ++i) {
-            if (num[i] == '0') {
-                pos = i;
+        int pos = num.find('0');
+        if (pos == string::npos)
+            pos = n - 1;
+
+        for (int i = 0; i < n; i++) {
+            if (num[i] == '0')
                 break;
-            }
-            rem[i + 1] = rem[i] / std::gcd(rem[i], (long long)(num[i] - '0'));
+
+            rem[i + 1] = rem[i] / gcd(rem[i], (long long)(num[i] - '0'));
         }
 
-        if (rem[n] == 1 && pos == n - 1 && num[pos] != '0')
+        if (num.find('0') == string::npos && rem[n] == 1)
             return num;
 
-        for (int i = pos; i >= 0; --i) {
-            int startDigit = (i < n ? (num[i] - '0') + 1 : 1);
+        for (int i = pos; i >= 0; i--) {
+            int startDigit = num[i] - '0' + 1;
 
-            for (int d = startDigit; d <= 9; ++d) {
-                long long now = rem[i] / std::gcd(rem[i], (long long)d);
+            for (int d = startDigit; d <= 9; d++) {
+                long long now = rem[i] / gcd(rem[i], (long long)d);
 
-                std::string suffix = "";
+                string suffix = "";
                 long long curr = now;
 
-                for (int j = n - 1; j > i; --j) {
-                    for (int v = 9; v >= 1; --v) {
+                for (int j = n - 1; j > i; j--) {
+                    bool found = false;
+
+                    for (int v = 9; v >= 2; v--) {
                         if (curr % v == 0) {
-                            suffix += std::to_string(v);
+                            suffix.push_back(char('0' + v));
                             curr /= v;
+                            found = true;
                             break;
                         }
                     }
+
+                    if (!found)
+                        suffix.push_back('1');
                 }
 
                 if (curr == 1) {
-                    std::reverse(suffix.begin(), suffix.end());
-                    return num.substr(0, i) + std::to_string(d) + suffix;
+                    reverse(suffix.begin(), suffix.end());
+                    return num.substr(0, i) + char('0' + d) + suffix;
                 }
             }
         }
 
         long long curr = t;
-        std::string digits = "";
+        string digits = "";
 
-        for (int v = 9; v >= 2; --v) {
+        for (int v = 9; v >= 2; v--) {
             while (curr % v == 0) {
-                digits += std::to_string(v);
+                digits.push_back(char('0' + v));
                 curr /= v;
             }
         }
 
-        std::sort(digits.begin(), digits.end());
+        if (curr != 1)
+            return "-1";
 
-        int reqLen = std::max(n + 1, (int)digits.length());
-        return std::string(reqLen - digits.length(), '1') + digits;
+        sort(digits.begin(), digits.end());
+
+        int reqLen = max(n + 1, (int)digits.size());
+
+        return string(reqLen - digits.size(), '1') + digits;
     }
 };
