5c06d1473eedd526c513ace262f19a493065e96b
Kaushal <kaushalkumar01956@gmail.com>
2026-09-08 15:28:03 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (97.78%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int upperCount=0;
+        for(char c:word){
+            if(isupper(c)) upperCount++;
+        }
+        if(upperCount==0 || upperCount==word.size()){
+            return true;
+        }
+        if(upperCount==1 && isupper(word[0])){
+            return true;
+        }
+        return false;
+    }
+};
