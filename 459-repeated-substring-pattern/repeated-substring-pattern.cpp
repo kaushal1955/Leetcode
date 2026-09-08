f5d49e030b358072d80feff1b401003e68398cc7
Kaushal <kaushalkumar01956@gmail.com>
2026-09-08 15:06:42 +0530

    Time: 55 ms (22.55%) | Memory: 94.8 MB (20.22%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        string doubled = (s + s).substr(1, 2 * s.size() - 2);
-        return doubled.find(s) != string::npos;
+       int n=s.size();
+       for(int i=1;i<=n/2;i++){
+        if(n%i==0){
+            string substring= s.substr(0,i);
+            string repeated="";
+            for(int j=0;j<n/i;j++){
+                repeated +=substring;
+            }
+            if(repeated==s)return true;
+        }
+       } 
+       return false;
     }
 };
